1a972fcc7073c11478ae607a354390c742454952
Paraschiv Tudor <gparaschiv@gmail.com>
2021-03-12 16:05:32 +0200

    da

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,5 +1,6 @@
 import sparse_matrix as spm
 import time
+import numpy as np
 
 a_matrix = spm.SparseMatrix()
 b_matrix = spm.SparseMatrix()
@@ -60,3 +61,20 @@ print("a+b == a+b", aplusb_matrix == aplusb_file_matrix)
 aorib_matrix = a_matrix * b_matrix
 print("a*b == a*b", aorib_matrix == aorib_file_matrix)
 print("--- %s seconds ---" % (time.time() - start_time))
+
+m1 = spm.SparseMatrix()
+m2 = spm.SparseMatrix()
+finalm = spm.SparseMatrix()
+parse_tri_matrix("mat1.txt", m1)
+parse_tri_matrix("mat2.txt", m2)
+finalm = m1 + m2
+
+with open('mat3.txt', 'w') as f:
+    mat = []
+    for line in finalm.values:
+        actual_line = [0 for j in range(finalm.rows + 1)]
+        for entry in line:
+            actual_line[entry[1]] = entry[0]
+        mat.append(actual_line)
+    f.write(str(np.array(mat)))
+    
